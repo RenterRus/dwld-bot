a18051199fc0dac1853c6e8c3d56628e4c5d68d0
RenterRus <artur.dawydov2013@yandex.ru>
2025-07-10 15:31:56 +0300

    part cases

--- internal/usecase/bot/bot.go
+++ internal/usecase/bot/bot.go
@@ -1,6 +1,8 @@
 package bot
 
 import (
+	"fmt"
+
 	"github.com/RenterRus/dwld-bot/internal/entity"
 	"github.com/RenterRus/dwld-bot/internal/repo/dwld"
 	"github.com/RenterRus/dwld-bot/internal/repo/persistent"
@@ -18,18 +20,23 @@ func NewDownloadUsecases(db persistent.SQLRepo, dwld dwld.DWLDModel) Bot {
 	}
 }
 
-func (b *BotCases) SetTask(entity.TaskModel) error {
-	// !!!
+func (b *BotCases) SetTask(task entity.TaskModel) error {
+	if err := b.db.SetTask(task); err != nil {
+		return fmt.Errorf("SetTask: %w", err)
+	}
+
 	return nil
 }
 
-func (b *BotCases) ViewTasks(userID string) ([]*entity.TaskModel, error) {
+func (b *BotCases) ViewTasks() ([]*entity.TaskModel, error) {
 	// !!!
+
 	return nil, nil
 }
 
-func (b *BotCases) DeleteTask(link, userID string) error {
+func (b *BotCases) DeleteTask(link string) error {
 	// !!!
+
 	return nil
 }
 
@@ -39,6 +46,9 @@ func (b *BotCases) Status() (*entity.TaskInfo, error) {
 }
 
 func (b *BotCases) StorageServer(server entity.ServerModel) error {
-	// !!!
+	if err := b.db.StorageServer(server); err != nil {
+		return fmt.Errorf("StorageServer: %w", err)
+	}
+
 	return nil
 }
--- internal/usecase/bot/model.go
+++ internal/usecase/bot/model.go
@@ -4,8 +4,8 @@ import "github.com/RenterRus/dwld-bot/internal/entity"
 
 type Bot interface {
 	SetTask(entity.TaskModel) error
-	ViewTasks(userID string) ([]*entity.TaskModel, error)
-	DeleteTask(link, userID string) error
+	ViewTasks() ([]*entity.TaskModel, error)
+	DeleteTask(link string) error
 	Status() (*entity.TaskInfo, error)
 
 	StorageServer(server entity.ServerModel) error
