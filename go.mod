719f7d32dc72454143f219e3c4bce9d6531fe6bc
Artur Davydov <a.davydov@zdravcity.ru>
2025-07-11 00:13:39 +0300

    init: delete msg

--- go.mod
+++ go.mod
@@ -18,6 +18,7 @@ require (
 	github.com/gabriel-vasile/mimetype v1.4.8 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
 	github.com/go-viper/mapstructure/v2 v2.2.1 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.3 // indirect
--- internal/repo/bot/bot.go
+++ internal/repo/bot/bot.go
@@ -1,5 +1,12 @@
 package bot
 
+import (
+	"fmt"
+	"strconv"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
 type BotRepo struct {
 }
 
@@ -8,6 +15,18 @@ func NewBotRepo() BotModel {
 }
 
 func (r *BotRepo) DeleteMsg(userID, messageID string) error {
-	// !!!
+	user := int64(0)
+	message := int64(0)
+	var err error
+
+	if user, err = strconv.ParseInt(userID, 10, 64); err == nil {
+		return fmt.Errorf("DeleteMsg.ParseInt(userID): %w", err)
+	}
+
+	if message, err = strconv.ParseInt(messageID, 10, 64); err == nil {
+		return fmt.Errorf("DeleteMsg.ParseInt(userID): %w", err)
+	}
+
+	tgbotapi.NewDeleteMessage(user, int(message))
 	return nil
 }
