9c5304c3ad0f16abc4e3d7d8093c5ee44eba0473
Artur Davydov <a.davydov@zdravcity.ru>
2025-07-18 01:26:18 +0300

    STAGE 1: nightly fix

--- cmd/main.go
+++ cmd/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/RenterRus/dwld-bot/internal/app"
 )
@@ -16,6 +17,8 @@ func main() {
 		log.Fatal("config flag not found")
 		os.Exit(1)
 	}
+
+	time.Sleep(time.Second)
 
 	if err := app.NewApp(*path); err != nil {
 		fmt.Println(err)
