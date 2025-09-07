e076003ed119f327ee98061200b207fa3ddf91dd
RenterRus <artur.dawydov2013@yandex.ru>
2025-09-07 19:11:13 +0300

    STAGE 1: remove restart

--- pkg/botserver/telegram.go
+++ pkg/botserver/telegram.go
@@ -2,17 +2,11 @@ package botserver
 
 import (
 	"context"
-	"fmt"
 	"time"
 
 	"github.com/RenterRus/dwld-bot/internal/controller/telegram"
 	"github.com/RenterRus/dwld-bot/internal/repo/persistent"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
-)
-
-// Поднимаем бота
-const (
-	LIFETIME = 1
 )
 
 type Bot struct {
@@ -33,21 +27,8 @@ func (b *Bot) Bot() *tgbotapi.BotAPI {
 }
 
 func (b *Bot) Start() {
-	var ctx context.Context
-	var cncl context.CancelFunc
-	ticker := time.NewTicker(time.Hour * LIFETIME)
-
-	go func() {
-		for {
-			fmt.Println("Bot starting")
-			ctx, cncl = context.WithCancel(context.Background())
-			go b.bot.Processor(ctx)
-			<-ticker.C
-			fmt.Println("Bot stopping")
-			cncl()
-			time.Sleep(time.Second * 5)
-		}
-	}()
+	ctx, cncl := context.WithCancel(context.Background())
+	go b.bot.Processor(ctx)
 
 	<-b.notify
 	cncl()
